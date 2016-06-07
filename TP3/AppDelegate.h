7fea81051fabeb74e4a3799d8bcce096401d124d
Jhonathan Santos <jhonathan.santos@sj208-06.local>
2016-06-07 19:15:49 -0300

    Initial Commit

---
+++ TP3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TP3
+//
+//  Created by Jhonathan Santos on 6/7/16.
+//  Copyright © 2016 Jhonathan Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
